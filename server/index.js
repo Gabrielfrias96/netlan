dae4c20dad1d52685c28da3d30ecf22494b8a9ab
gabrielfrias96 <gabriel.frias1996@gmail.com>
2022-04-22 17:05:13 -0300

    Re-deploy

--- server/index.js
+++ server/index.js
@@ -3,16 +3,21 @@ import bodyParser from 'body-parser'
 import {usersRouter} from './routes/users.route.js'
 import {connectDB} from './db.js'
 import { port } from './config.js';
+import {dirname , join} from 'path'
 
 
 const app = express()
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 connectDB()
 
-app.listen( port || 5000 , ()=> console.log("Servidor Funcionando")) 
-
-
 app.use(usersRouter) 
+
+app.use(express.static(join(__dirname, '../client/build')))
+
+
+app.listen( port || 5000 , ()=> console.log("Servidor Funcionando")) 
